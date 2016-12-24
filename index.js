fe7dbec4f9d92264635c27ff86e954589801d306
veeramarni <mvasireddy@cdmbase.com>
2016-12-24 18:22:33 -0500

    added ssr

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-var isBrowser=new Function("try {return this===window;}catch(e){ return false;}");
+const isBrowser = new Function('try {return this===window;}catch(e){ return false;}');
 
-var isServerSideRenderEnabled = new Function("try {return this===process.browser;}catch(e){ return false;}");
+const isServerSideRenderEnabled = new Function('try {return this===process.browser;}catch(e){ return false;}');
 
-var isNode=new Function("try {return this===global;}catch(e){return false;}");
+const isNode = new Function('try {return this===global;}catch(e){return false;}');
 
+const isSSR = isServerSideRenderEnabled();
 
-if(isBrowser() || isServerSideRenderEnabled() ){
-    module.exports = require('./src/client.js');
-} else if(isNode()){
-    module.exports = require('./src/server')
+if (isBrowser() || isSSR) {
+  if (isSSR) {
+    console.log('Enabling cdm-logger for server side rendering');
+  }
+  module.exports = require('./src/client.js');
+} else if (isNode()) {
+  module.exports = require('./src/server');
 } else {
-    console.log("Logger Error: Cannot determine whether the environment as browser or server");
+  console.log('Logger Error: Cannot determine whether the environment as browser or server');
 }
 
